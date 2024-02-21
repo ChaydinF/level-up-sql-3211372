09d991439a48603fbc16c3b47ee95998ffc02394
ChaydinF <136875034+ChaydinF@users.noreply.github.com>
2024-02-21 18:51:49 +0000

    modified:   .vscode/settings.json 	modified:   01_restaurant/challenges/01_01_invitations_ch.sql 	modified:   01_restaurant/challenges/01_02_anniversary_ch.sql 	modified:   01_restaurant/challenges/01_03_printmenus_ch.sql 	modified:   01_restaurant/challenges/01_04_newcustomer_ch.sql 	modified:   01_restaurant/challenges/01_05_updateinfo_ch.sql 	modified:   01_restaurant/challenges/01_06_removeinfo_ch.sql  modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,22 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT 
+  Name, Description, Price, Type
+FROM Dishes
+ORDER BY Price ASC;
+
+SELECT 
+  Name, Description, Price, Type
+FROM Dishes
+WHERE TYPE== 'Appetizer' OR TYPE== 'Beverage'
+ORDER BY Type; 
+
+SELECT 
+  Name, Description, Price, Type
+FROM Dishes
+WHERE TYPE!= 'Beverage'
+ORDER BY Type; 
+
+--El ORDER BY fue idea del tutor y tiene razon siempre se muestran en orden
--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,36 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+FROM Customers
+WHERE FIRSTNAME= 'Norby';
+
+
+/*SELECT *
+FROM Reservations
+WHERE CustomerID=64
+--AND Date LIKE '2022-07%';
+AND Date > '2022-07-24';*/
+
+SELECT R.*
+FROM Reservations R
+JOIN Customers C 
+  ON C.CustomerID = R.CustomerID
+WHERE C.FirstName='Norby'
+AND Date > '2022-07-24';
+
+/* 
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000
+
+borrar registros puede crear inconsistencias si
+otra tabla tiene en uso el registro, lo mejor es hacer update
+de uno más columnas del registro
+
+*/
+
+UPDATE Reservations
+SET PartySize = 0
+WHERE ReservationID = 2000
