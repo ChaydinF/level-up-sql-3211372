49696e6843c11fb06cf855985d8911402ddab6cb
ChaydinF <136875034+ChaydinF@users.noreply.github.com>
2024-02-27 18:01:18 +0000

    modified:   01_restaurant/challenges/01_07_attendevent_ch.sql 	modified:   01_restaurant/challenges/01_08_findreservation_ch.sql 	modified:   01_restaurant/challenges/01_09_createreservation_ch.sql 	modified:   01_restaurant/restaurant.db

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,12 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT 
+  *
+FROM Reservations R 
+INNER JOIN Customers C
+ON C.CustomerID= R.CustomerID
+WHERE C.LastName LIKE 'STE%'
+AND PARTYSIZE= 4
+AND DATE LIKE '2022-06-14%'
