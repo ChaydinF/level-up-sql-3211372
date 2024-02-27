03f516a3d9db7afb08e4222be69eca9ce5fbf2d3
ChaydinF <136875034+ChaydinF@users.noreply.github.com>
2024-02-27 19:56:05 +0000

    modified:   02_library/challenges/02_05_returnbooks_ch.sql 	modified:   02_library/library.db

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -7,12 +7,38 @@
 SELECT
   * 
 FROM Books
-WHERE Barcode IN ('6435968624','5677520613','8730298424')
+WHERE Barcode IN ('6435968624','5677520613','8730298424');
+
+SELECT
+  *
+FROM Loans L;
 
 SELECT
   *
 FROM Loans L
-JOIN Books B
+JOIN Books B 
 ON B.BookID=L.BookID
 WHERE B.Barcode IN ('6435968624','5677520613','8730298424')
-AND 
+--AND L.ReturnedDate IS NULL;
+AND L.ReturnedDate= '2022-07-05';
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE LoanID IN (SELECT
+                    LoanID
+                  FROM Loans L
+                  JOIN Books B 
+                  ON B.BookID=L.BookID
+                  WHERE B.Barcode IN ('6435968624','5677520613','8730298424')
+                  AND L.ReturnedDate IS NULL);
+
+/*Solución chatgpt*/
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05' -- Fecha deseada de retorno
+WHERE BookID IN (
+    SELECT BookID
+    FROM Books
+    WHERE Barcode IN ('6435968624', '5677520613', '8730298424')
+)
+AND ReturnedDate IS NULL;
